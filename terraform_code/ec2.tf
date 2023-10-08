73e818fd67010787509f0e1718e34cab7b0d75a2
hemanth <dkrhemanth@gmail.com>
2023-10-08 17:31:15 +0530

    adding jenkins port 8080 to the security group

--- terraform_code/ec2.tf
+++ terraform_code/ec2.tf
@@ -29,6 +29,13 @@ resource "aws_security_group" "devops-project1-sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     }
+  ingress {
+    description      = "Jenkins Port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }  
 
   egress {
     from_port        = 0
